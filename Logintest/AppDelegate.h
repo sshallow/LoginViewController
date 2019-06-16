425679b78a32b1a2225df2f8f9a218f60d88d49b
sshallow <doit_2017@163.com>
2019-06-16 23:02:27 +0800

    Initial Commit

---
+++ Logintest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Logintest
+//
+//  Created by apple on 2019/6/16.
+//  Copyright © 2019 Anti-wolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Logintest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Logintest
+//
+//  Created by apple on 2019/6/16.
+//  Copyright © 2019 Anti-wolf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
